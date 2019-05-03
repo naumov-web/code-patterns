b7417263612cbfb54428892226c9609481ef2e10
Константин Наумов <konstantin@MacBook-Pro-Konstantin.local>
2019-05-03 14:40:53 +0300

    Sceletone on Decorator pattern

---
+++ Decorator/DecoratorExample.java
@@ -0,0 +1,20 @@
+public class DecoratorExample {
+
+    /**
+     * Выполнить программу
+     */
+    public void run() {
+        System.out.println("Decorator pattern");
+    }
+
+    /**
+     * Главная точка входа
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        DecoratorExample app = new DecoratorExample();
+        app.run();
+    }
+
+}
